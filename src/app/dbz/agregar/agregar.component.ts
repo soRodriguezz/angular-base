77abbe914d8de7b70ffb4d71d6250a27058d7435
soRodriguezz <s.rodriguezzapata01@gmail.com>
2021-02-21 15:35:56 -0300

    neuvo heroe

---
+++ src/app/dbz/agregar/agregar.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interfaces';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+})
+export class AgregarComponent {
+
+  @Input() nuevo: Personaje = {
+    nombre: '',
+    poder: 0,
+  };
+
+  agregar() {
+    if (this.nuevo.nombre.trim().length === 0) {
+      return;
+    }
+
+    console.log(this.nuevo);
+
+    this.nuevo = {
+      nombre: '',
+      poder: 0,
+    };
+  }
+}
